c98bc8c898cc40d355e6c15ac83935a711224875
behrooz mohamadi <b.mohammadi@kian.digital>
2022-08-11 14:53:49 +0430

    add message description

--- src/main/kotlin/com/example/kotlinspring/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/example/kotlinspring/exception/ResourceNotFoundException.kt
@@ -3,5 +3,5 @@ package com.example.kotlinspring.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 
-class ResourceNotFoundException(reason: String?): ResponseStatusException(HttpStatus.NOT_FOUND,reason) {
+class ResourceNotFoundException(reason: String?): ResponseStatusException(HttpStatus.NOT_FOUND, reason) {
 }
